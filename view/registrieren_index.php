f7f9ec61eb9ea86a4393ece2fad0a9f911e5453e
aaronmeier10 <aaron.uferweg@gmail.com>
2016-03-09 15:46:34 +0100

    Registration verbessert

--- view/registrieren_index.php
+++ view/registrieren_index.php
@@ -2,29 +2,86 @@
 <html lang="de">
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="../view/css/login_style.css">
  <link href="../view/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../view/css/login_style.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <title>Kochtopf - Registrieren</title>
 
 	
 <script type="text/javascript">
-function validate(){
-	var jqxhr = $.ajax( "../model/UserModel.php" )
-	  .done(checkUsername(document.getElementsByTagName("benutzername").value) {
-	    alert( "success" );
-	    return false;
-	  })
-}
-}
+$(function(){
+$("form").submit(function(event){
+	if($('.validation_nok').length > 0){
+		event.preventDefault();
+	}
+/*	var jqxhr = $.ajax( "../model/RegistrierenModel.php?n="+$('#benutzername').val())
+	.done(function(data) {
+		var pw_pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/i;
+		if(data == 'true'){
+			if($('#passwort_w').val() == $('#passwort').val()){
+				if($('#benutzername').val().length > 3 && $('#benutzername').val().length < 20){
+					if($('#passwort').val().match(pw_pattern)){
+						alert("Success");
+						
+					}else{
+						event.preventDefault();
+						alert("Passwort muss min 1 Buchstaben, 1 Zahl und 1 Sonderzeichen enthalten");
+					}
+				}else{
+					event.preventDefault();
+					alert("Benutzername muss zwischen 3 und 20 Zeichen lang sein");
+				}
+			}else{
+				event.preventDefault();
+				alert("Passwort wurde nicht richtig widerholt");
+			}
+		}else{
+			event.preventDefault();
+			alert( data );
+		}
+	});*/
+});
+	$('#benutzername').change(function () {
+		if($('#benutzername').val().length > 3){
+			$('#benutzername').removeClass('validation_nok');
+			$('#benutzername').removeClass('validation_ok');
+			var jqxhr2 = $.ajax( "../model/RegistrierenModel.php?n="+$('#benutzername').val())
+			.done(function(data){
+				if(data == 'true'){
+					$('#benutzername').addClass("validation_ok");
+				}else{
+					$('#benutzername').addClass("validation_nok");
+				}
+			});
+		}else{
+			$('#benutzername').removeClass('validation_nok');
+			$('#benutzername').removeClass('validation_ok');
+		}
+			
+	});
+
+	$('#passwort_w').change(function () {
+		$('#passwort_w').removeClass('valdiation_nok');
+		$('#passwort_w').removeClass('validation_ok');
+		$('#passwort').removeClass('valdiation_nok');
+		$('#passwort').removeClass('validation_ok');
+		if($('#passwort_w').val() == $('#passwort').val()){
+			$('#passwort_w').addClass("validation_ok");
+			$('#passwort').addClass("validation_ok");
+		}else{
+			$('#passwort_w').addClass("validation_nok");
+			$('#passwort').addClass("validation_nok");
+		};
+	});
+});
 </script>
 </head>
 <body>
 	<div class="main_r">
-			<form action="User/" method="post" onsubmit="return validate()" name="form">
+			<form action="/User/doCreate" method="post" name="form">
 				<h3>Registration</h3>
 				<p>Benutzername</p>
-				<input name="benutzername" type="text" class="form-control" placeholder="Benutzername" required="required">
+				<input name="benutzername" id="benutzername" type="text" class="form-control" placeholder="Benutzername" required="required">
 				</br>
 				<p>Vorname</p>
 				<input name="vorname" type="text" class="form-control" placeholder="Vorname" required="required">
@@ -39,15 +96,15 @@ function validate(){
 				<input name="alter" type="number" class="form-control" placeholder="Alter" required="required">
 				</br>
 				<p>Geschlecht</p>
-				<label class="radio-inline"><input type="radio" name="optradio">Männlich</label>
-				<label class="radio-inline"><input type="radio" name="optradio">Weblich</label>
+				<label class="radio-inline"><input type="radio" name="alter">Männlich</label>
+				<label class="radio-inline"><input type="radio" name="alter">Weblich</label>
 				</br>
 				</br>
 				<p>Passwort</p>
-				<input name="passwort" type="password" class="form-control" placeholder="Passwort" required="required">
+				<input id="passwort" name="passwort" type="password" class="form-control" placeholder="Passwort" required="required">
 				</br>
 				<p>Passwort wiederholen</p>
-				<input name="passwort_wiederholen" type="password" class="form-control" placeholder="Passwort wiederholen" required="required">
+				<input id="passwort_w" name="passwort_wiederholen" type="password" class="form-control" placeholder="Passwort wiederholen" required="required">
 				</br>
 				</br>
 				<button type="submit" class="btn btn-success">Registrieren</button>
