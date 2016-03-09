a017cca17a48cc377ca12990a4493f1dfb492bd4
Keller Anuraly <bkella@bbcnet.ch>
2016-03-09 16:13:55 +0100

    zurück taste angepasst

--- view/homeAnzeige_index.php
+++ view/homeAnzeige_index.php
@@ -1,11 +1,20 @@
-<div id="inhalt" class="container-fluid">
+<div id="inhalt-homeAnzeige" class="container-fluid">
      <?php if (empty($rezeptAnzeigen=$rezept[0])): ?>
 		    <div class="dhd">
 			    <h2 class="item title">Hoopla! Rezept nicht gefunden.</h2>
 		    </div>
 	    <?php else: ?>
-                <div id="aussen-zelle" class="container">
-			        <div id="innere-zelle" class="container">
+                <div id="head-inhalt-homeAnzeige" class="container">
+                    <?= $rezeptAnzeigen->titel;?> <?= $benutzerAnzeigen->benutzername;?>
+                </div>
+			    <div id="body-inhalt-homeAnzeige" class="container">
+                    <?= $rezeptAnzeigen->beschreibung;?>
+                </div>
+                <div id="footer-inhalt-homeAnzeige" class="container">
+                    <a href="/home" id="zurück-button-homeAnzeige" type="button" class="btn btn-success">Zurück</a>
+                    
+                    <a href="" id="kommentieren-button-homeAnzeige" type="button" class="btn btn-success">Kommentieren</a>
+                </div>
                         <table>
                             <tr>
                                 <td><?= $rezeptAnzeigen->titel;?></td>
@@ -14,8 +23,6 @@
                                 <td><?= $rezeptAnzeigen->beschreibung;?></td> 
                             </tr>
 				        </table>
-                    </div>  
-                </div>
 	<?php endif ?>
                         
 </div>
